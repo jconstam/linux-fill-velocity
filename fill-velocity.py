7e2cd53d5309eadf42d5e7b8b7218afc2c892828
Jason Constam <jconstam@gmail.com>
2022-12-10 20:20:25 -0700

    Added default port

--- fill-velocity.py
+++ fill-velocity.py
@@ -4,7 +4,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fill Velocity Monitor')
-    parser.add_argument('-p', '--port', type=int, required=True, help='TCP port for Web UI')
+    parser.add_argument('-p', '--port', type=int, default=8080, help='TCP port for Web UI')
     parser.add_argument('disks', metavar='D', type=str, nargs='+', help='Paths to disks to be monitored')
     args = parser.parse_args()
 
